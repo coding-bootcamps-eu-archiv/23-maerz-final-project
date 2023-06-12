b2e169a9bc77b6c8c2ecc498cab1ef9c642869a9
Katharina Endner <k.endner@gmail.com>
2023-06-12 12:45:50 +0200

    feat: adds a footer

---
+++ src/components/home-view/FooterArea.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer id="Footer-Links"> 
+<div id="links">
+  <a href="">Terms & Conditions</a>
+  <a href=""> 🛈 About</a>
+  <a href=""> 🍪 Cookie Settings</a>
+  <a href="">✉️ Contact</a>
+</div>
+</footer>
+
+
+</template>
+
+<style scoped>
+*{
+    font-family: sans-serif;
+}
+#Footer-Links{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: lightgrey;
+
+}
+#links{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0rem;
+}
+a{
+    padding: 0.1rem 0rem;
+    color: black;
+    font-weight: bold;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,9 @@
 
 <template>
   <main><h1>Arcade</h1></main>
+  <FooterArea />
 </template>
+
+<script setup>
+import FooterArea from "@/components/home-view/FooterArea.vue"
+</script>
