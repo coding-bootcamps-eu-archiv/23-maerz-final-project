7d94899756cd5abfde607c25335f87fc7f514ba2
Annelie Wachstein <anwachstein@gmail.com>
2023-06-12 16:12:12 +0200

    feat: import Header component to App.vue and remove navigation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Header from "@/components/home-view/Header.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <Header></Header>
   <RouterView />
 </template>
 
