51fdc27c761709bfdaeefebf33c32e7c49944ddc
Kai Weluda <kaiweluda@gmail.com>
2023-06-13 13:02:57 +0200

    chore: adds temporary changes to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
-  <RotatingGallery></RotatingGallery>
+  <!-- <RotatingGallery></RotatingGallery> -->
+  <TetrisGame></TetrisGame>
   <FooterArea />
 </template>
 
 <script setup>
 import FooterArea from "@/components/home-view/FooterArea.vue";
 import RotatingGallery from "../components/home-view/RotatingGallery.vue";
+import TetrisGame from "../components/tetris/TetrisGame.vue";
 </script>
