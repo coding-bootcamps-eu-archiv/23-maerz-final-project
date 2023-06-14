f70e60aae61e45b9310725f9fe3ee8eea9bd5e94
Annelie Wachstein <anwachstein@gmail.com>
2023-06-14 13:07:46 +0200

    feat: implement game data and generating card for each game

--- src/components/all-games-view/AllGames.vue
+++ src/components/all-games-view/AllGames.vue
@@ -1,7 +1,37 @@
-<template></template>
+<template>
+  <div class="all-cards-wrapper">
+    <GameCard v-for="game in games" :name="game.name"></GameCard>
+  </div>
+</template>
 
 <script setup>
 import GameCard from "@/components/all-games-view/GameCard.vue";
+import { ref } from "vue";
+
+// data
+const games = ref([
+  {
+    id: 1,
+    name: "memory",
+    img: "dsdjkfdsf",
+    link: "",
+  },
+  {
+    id: 2,
+    name: "rock paper scissors",
+    img: "jfhjgh",
+    link: "",
+  },
+]);
 </script>
 
-<style scoped></style>
+<style scoped>
+.all-cards-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 2rem;
+  gap: 2rem;
+  background-color: var(--primary-dark);
+}
+</style>
